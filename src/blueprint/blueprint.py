d4260fd61b737e8024e6dd25720143e932d482eb
Caleb Ely <le717@users.noreply.github.com>
2019-01-05 15:07:34 -0500

    Add initial blueprint file

---
+++ src/blueprint/blueprint.py
@@ -0,0 +1,23 @@
+
+from datetime import datetime
+
+from flask import Blueprint, request
+from flask import render_template, url_for
+
+bp = Blueprint("root", __name__, url_prefix="")
+
+
+@bp.route("/form", methods=["POST"])
+def form() -> str:
+    return "form"
+
+@bp.route("/")
+@bp.route("/today")
+def index() -> str:
+    date = datetime.today().strftime("%d %B, %Y")
+    return render_template("word.html", date=date)
+
+
+@bp.route("/<date>")
+def date(date) -> str:
+    return render_template("word.html", date="")
