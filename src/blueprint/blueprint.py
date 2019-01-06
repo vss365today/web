8e77942536eae28d9de90cb7c8a231e58752ccfc
Caleb Ely <le717@users.noreply.github.com>
2019-01-05 20:05:41 -0500

    Routing, filters, templates

--- src/blueprint/blueprint.py
+++ src/blueprint/blueprint.py
@@ -1,8 +1,11 @@
-
 from datetime import datetime
+from datetime import timedelta
 
-from flask import Blueprint
+from flask import Blueprint, request
 from flask import render_template
+
+from src.core.tweets import get_tweet_today, get_tweet_by_date, find_image
+
 
 bp = Blueprint("root", __name__, url_prefix="")
 
@@ -15,10 +18,38 @@ def form() -> str:
 @bp.route("/")
 @bp.route("/today")
 def index() -> str:
-    date = datetime.today().strftime("%d %B, %Y")
-    return render_template("word.html", date=date)
+    tweet = get_tweet_today()
+    return render_template("word.html", tweet=tweet)
 
 
 @bp.route("/<date>")
 def date(date) -> str:
-    return render_template("word.html", date="")
+    tweet = get_tweet_by_date(date)
+    return render_template("word.html", tweet=tweet)
+
+@bp.app_errorhandler(404)
+def page_not_found(e) -> str:
+    # Create a dummy tweet object containing the requested date
+    tweet = {}
+    tweet["date"] = datetime.strptime(request.path[1:], "%Y-%m-%d")
+    return render_template("404.html", tweet=tweet), 404
+
+@bp.app_template_filter()
+def format_date(date) -> str:
+    return date.strftime("%d %B, %Y")
+
+@bp.app_template_filter()
+def format_content(content) -> str:
+    return "\n".join([
+        f"<p>{find_image(para)}</p>"
+        for para in content.split("\r\n")
+        if para
+    ])
+
+@bp.app_template_filter()
+def yesterday(date) -> str:
+    return datetime.strftime(date - timedelta(1), "%Y-%m-%d")
+
+@bp.app_template_filter()
+def tomorrow(date) -> str:
+    return datetime.strftime(date + timedelta(1), "%Y-%m-%d")
