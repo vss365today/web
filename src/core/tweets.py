af91e917d87fa77a366d666f360d99c3e308fd62
Caleb Ely <le717@users.noreply.github.com>
2019-01-05 20:06:14 -0500

    Code to pull tweets and render them

--- src/core/tweets.py
+++ src/core/tweets.py
@@ -1,1 +1,32 @@
-import tweepy
+import datetime
+import re
+
+# import tweepy
+from src.extensions import alchemy
+from src.models import Tweets
+
+
+def find_image(content):
+    regex = re.compile(r"https://.+\.(?:jpg|png|gif)", re.MULTILINE)
+    match = re.search(regex, content)
+    if match:
+        url = match.group(0)
+        content = content.replace(url, f'<img width="500" src="{url}">')
+    return content
+
+
+def format_tweet(tweet):
+    # Format the tweet content, attempting to display any images
+    tweet.content = "\n".join([
+        f"<p>{find_image(para)}</p>"
+        for para in tweet.content.split("\r\n")
+        if para
+    ])
+    return tweet
+
+
+def get_tweet_today():
+    return Tweets.query.order_by(Tweets.date.desc()).first()
+
+def get_tweet_by_date(date):
+    return Tweets.query.filter(Tweets.date.startswith(date)).first_or_404()
