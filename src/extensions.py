593c2367307144e2dac12ea7a2f2712b1ba70c7e
Caleb Ely <le717@users.noreply.github.com>
2019-01-05 23:49:00 -0500

    Make use of helper methods

--- src/extensions.py
+++ src/extensions.py
@@ -1,34 +1,29 @@
-import os
-
-from dotenv import dotenv_values, find_dotenv
-from sqlalchemy import create_engine
-
 # from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
+
+from src.core.twitter import Twitter
+from src.core.helpers import create_db_connection, load_env_vals
 
 
 alchemy = SQLAlchemy()
 csrf = CSRFProtect()
 db = None
 # email = Mail()
+twitter = Twitter()
 
 
 def init_extensions(app):
     global db
 
-    # Load the variables from the .env file into the app config
-    env_vals = dotenv_values(find_dotenv())
-    for key, value in env_vals.items():
-        app.config[key] = (value if value != "" else None)
-
-    # Load app exensions
+    # Load app extensions
+    app.config.update(load_env_vals())
     csrf.init_app(app)
     # email.init_app(app)
+    twitter.init_app(app)
 
     # Set the database connection
-    base_dir = os.path.abspath(os.path.dirname(__file__))
-    app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:@localhost/vss365"
+    connect_str, db = create_db_connection(app.config)
+    app.config["SQLALCHEMY_DATABASE_URI"] = connect_str
     alchemy.init_app(app)
-    db = create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
     db.connect()
