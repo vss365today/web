7b988c4d006145ee5dcc7f0c5fa2e3ba8b0d02e9
Caleb Ely <le717@users.noreply.github.com>
2019-01-11 00:02:22 -0500

    Reworking email generator to actually work

--- src/core/emails/generator.py
+++ src/core/emails/generator.py
@@ -1,20 +1,29 @@
+from os.path import abspath
+
 from flask import render_template, render_template_string
 
-from src.core. filters import format_content, format_date
+from src.core.filters import (
+    create_date,
+    format_content,
+    format_date
+)
 
 
 def render_email_base(tweet: dict) -> str:
     # Get the site/email CSS
-    with open("../../static/style.css", "rt") as f:
+    with open(abspath("src/static/style.css"), "rt") as f:
         css_styles = f.read()
 
     # Render the base email content
     render_opts = {
-        "date": format_date(tweet.date),
-        "content": format_content(tweet.content),
+        "date": format_date(create_date(tweet["date"])),
+        "content": format_content(tweet["content"]),
         "css_styles": css_styles
     }
-    return render_template("../../templates/email.html", **render_opts)
+    return render_template(
+        abspath("src/templates/email.html"),
+        **render_opts
+    )
 
 
 def render_email_addr(template: str, email: str) -> str:
