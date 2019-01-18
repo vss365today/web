2f071bddee40710a88bd32241579786cbb472c9f
Caleb Ely <le717@users.noreply.github.com>
2019-01-17 22:58:59 -0500

    Begin better word prompt formatting

--- src/core/filters.py
+++ src/core/filters.py
@@ -3,6 +3,7 @@ import re
 
 
 __all__ = [
+    "create_curly_quotes",
     "create_date",
     "create_proper_image_url",
     "format_content",
@@ -11,6 +12,68 @@ __all__ = [
     "render_template",
     "render_template_string"
 ]
+
+
+def __curly_replace_double_quotes(text):
+    """https://stackoverflow.com/a/44336227"""
+    # 1. Replace "space followed by straight double quote"
+    # by "space followed by opening curly":
+    text = text.replace(' "', " “")
+
+    # 2. Replace "straight double quote followed by space"
+    # by "closing curly followed by space":
+    text = text.replace('" ', "” ")
+
+    # 3. Replace "straight double quote followed by comma"
+    # by "closing curly followed by comma":
+    text = text.replace('",', "”,")
+
+    # 4. Replace "straight double quote followed by semicolon"
+    # by "closing curly followed by semicolon":
+    text = text.replace('";', "”;")
+
+    # 5. Replace "straight double quote followed by fullstop"
+    # by "closing curly followed by fullstop":
+    text = text.replace('".', "”.")
+    return text
+
+
+def __curly_replace_single_quotes(text):
+    """https://stackoverflow.com/a/44336227"""
+    # 1. Replace "space followed by straight single quote"
+    # by "space followed by opening curly":
+    text = text.replace(" '", " ‘")
+
+    # 2. Replace "straight single quote followed by space"
+    # by "closing curly followed by space":
+    text = text.replace('" ', "” ")
+
+    # 3. Replace "straight single quote followed by comma"
+    # by "closing curly followed by comma":
+    text = text.replace("',", "’,")
+
+    # 4. Replace "straight single quote followed by semicolon"
+    # by "closing curly followed by semicolon":
+    text = text.replace("';", "’”;")
+
+    # 5. Replace "straight single quote followed by fullstop"
+    # by "closing curly followed by fullstop":
+    text = text.replace("'.", "’.")
+    return text
+
+
+def create_curly_quotes(text):
+    # text = body[0].decode()
+
+    # https://stackoverflow.com/a/44336227
+    # https://stackoverflow.com/a/2202835
+    # regex_single_quotes = re.compile(r"", re.MULTILINE)
+    # regex_double_quotes = re.compile(r"", re.MULTILINE)
+    text = __curly_replace_double_quotes(text)
+    text = __curly_replace_single_quotes(text)
+
+    # body[0] = text.encode()
+    return text
 
 
 def create_date(date_l: list) -> date:
@@ -24,6 +87,35 @@ def create_proper_image_url(
 ) -> str:
     """Replace a t.co image short link with a full image url."""
     return text.replace(img_short_url, img_full_url)
+
+
+def find_prompt_word(text: str) -> str:
+    prompt_word = ""
+
+    # Find all hashtags in the tweet
+    regex = re.compile(
+        r"(#\w+)",
+        re.MULTILINE
+    )
+    matches = re.findall(regex, text)
+
+    # We have hashtags
+    if matches:
+        remaining = list(filter(
+            lambda hashtag: hashtag.upper() not in ("#VSS365", "#PROMPT"),
+            matches
+        ))
+
+        # If there are any hashtags left, get the first one
+        # and remove the prefixed pound sign
+        if remaining:
+            prompt_word = remaining[0]
+    return prompt_word
+
+
+def format_prompt_word(word: str) -> str:
+    # TODO: Do this better
+    return f"<h3>{word}</h3>"
 
 
 def format_image_url(text: str) -> str:
@@ -42,11 +134,23 @@ def format_image_url(text: str) -> str:
 
 
 def format_content(text: str) -> str:
-    return "\n".join([
-        f"<p>{format_image_url(para)}</p>"
-        for para in text.split("\n")
-        if para
-    ])
+    # Start by forming a proper image URL
+    text = format_image_url(text)
+
+    # Get the prompt word, format it differently,
+    # and put it back into the content
+    prompt_word = find_prompt_word(text)
+    formatted_word = format_prompt_word(prompt_word)
+    text = text.replace(prompt_word, formatted_word)
+
+    # Wrap all non-blank lines in paragraphs
+    split_text = text.split("\n")
+    split_text = [
+        f"<p>{para}</p>"
+        for para in split_text
+        if para  # false-y value means blank line
+    ]
+    return "\n".join(split_text)
 
 
 def format_date(date) -> str:
