ddf1d2cbc989bbecb28b7a52bd3c3d8e97b8f17a
Caleb Ely <le717@users.noreply.github.com>
2019-01-26 14:24:21 -0500

    Hide subscription form (for now)

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from datetime import date
 from flask import Flask
 
 from src.blueprint import blueprint
+from src.core.filters import create_tweet_url
 from src.extensions import init_extensions
 
 
@@ -17,5 +18,9 @@ def create_app():
     @app.context_processor
     def inject_current_year():
         return {"current_year": date.today().year}
+
+    @app.context_processor
+    def create_url():
+        return {"create_url": lambda tweet: create_tweet_url(tweet)}
 
     return app
