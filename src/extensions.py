33074c80385abf5859ed697b5b965fc7ec07ea66
Caleb Ely <le717@users.noreply.github.com>
2019-06-03 19:47:35 -0400

    So that's why I was loading the env into the app lol

--- src/extensions.py
+++ src/extensions.py
@@ -1,4 +1,5 @@
 from flask_wtf.csrf import CSRFProtect
+from src.core.config import load_app_config
 from src.core.database import create_new_database
 
 
@@ -7,6 +8,7 @@ csrf = CSRFProtect()
 
 def init_extensions(app):
     # Load app extensions
+    app.config.update(load_app_config())
     csrf.init_app(app)
 
     # If a database is needed, one will be made
