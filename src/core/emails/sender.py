8ddcca742147879322a94c9f80270206be14a0e1
Caleb Ely <le717@users.noreply.github.com>
2019-06-24 20:49:27 -0400

    Send emails in chunks of 50 addresses

--- src/core/emails/sender.py
+++ src/core/emails/sender.py
@@ -33,31 +33,50 @@ def send_emails(tweet: dict):
     # Properly format the tweet date
     tweet["date"] = format_date(tweet["date"])
 
-    # Get the email address list
+    # Get the email address list and break it into chunks of 50
+    # The MailJet Send API, under a free account,
+    # has a limit of 50 addresses per batch, up to 200 emails a day.
+    # If/when there are more than 50 emails in the db,
+    # we need to chunk the addresses. This will chunk them
+    # into a nth-array level containing <= 50 emails.
+    # That said, if there is ever > 200 emails,
+    # MailJet just won't cut it anymore. :sad_face:
+    chunk_size = 50
     email_list = get_all_emails()
+    email_list = [
+        email_list[i:i + chunk_size]
+        for i in range(0, len(email_list), chunk_size)
+    ]
 
-    # Construct the emails
-    email_data = {"Messages": []}
-    for addr in email_list:
-        msg = construct_email(tweet, addr)
-        email_data["Messages"].append(msg)
+    for chunk in email_list:
+        # Construct the emails in each chunk
+        email_data = {"Messages": []}
+        for addr in chunk:
+            msg = construct_email(tweet, addr)
+            email_data["Messages"].append(msg)
 
-    # Send the emails via MailJet
-    result = mailjet.send.create(data=email_data)
-    print(f"Mail status: {result.status_code}")
+        # Send the emails
+        result = mailjet.send.create(data=email_data)
+        print(f"Mail status: {result.status_code}")
 
-    # Get the sending results json
-    mj_results = result.json()
+        # Get the sending results json
+        mj_results = result.json()
 
-    # Count the send status of each message
-    status = {}
-    for msg in mj_results["Messages"]:
-        status[msg["Status"]] = status.get(msg["Status"], 0) + 1
+        # There was an error sending the emails
+        if result.status_code != 200:
+            print(mj_results)
 
-    # All the emails were send successfully
-    if status["success"] == len(email_list):
-        print("All emails sent successfully.")
+        # The emails were successfully sent
+        else:
+            # Count the send status of each message
+            status = {}
+            for msg in mj_results["Messages"]:
+                status[msg["Status"]] = status.get(msg["Status"], 0) + 1
 
-    # Everything wasn't successful, display raw count dict
-    else:
-        print(status)
+            # All the emails were send successfully
+            if status["success"] == len(email_list):
+                print("All emails sent successfully.")
+
+            # Everything wasn't successful, display raw count dict
+            else:
+                print(status)
