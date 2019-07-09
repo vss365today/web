2b1e897fdd6665719f6d2e262462cde48c8aebf6
Caleb Ely <le717@users.noreply.github.com>
2019-07-09 14:49:19 -0400

    Update database schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,11 +4,19 @@ CREATE TABLE IF NOT EXISTS "emails" (
   PRIMARY KEY("email")
 );
 
-CREATE TABLE IF NOT EXISTS "givers" (
+CREATE TABLE IF NOT EXISTS "writers" (
   "uid" VARCHAR(30) NOT NULL UNIQUE,
   "handle" VARCHAR(20) NOT NULL,
-  "date" VARCHAR(7) NOT NULL UNIQUE,
   PRIMARY KEY("uid")
+);
+
+CREATE TABLE IF NOT EXISTS "writer_dates" (
+  "uid" VARCHAR(30) NOT NULL,
+  "date" VARCHAR(7) NOT NULL,
+  PRIMARY KEY("uid","date"),
+  FOREIGN KEY ("uid") REFERENCES writers("uid")
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT
 );
 
 CREATE TABLE IF NOT EXISTS "tweets" (
@@ -19,7 +27,7 @@ CREATE TABLE IF NOT EXISTS "tweets" (
   "word" VARCHAR(25) NOT NULL,
   "media" VARCHAR(512),
   PRIMARY KEY("tweet_id"),
-  FOREIGN KEY ("uid") REFERENCES givers("uid")
+  FOREIGN KEY ("uid") REFERENCES writers("uid")
     ON UPDATE CASCADE
     ON DELETE RESTRICT
 );
