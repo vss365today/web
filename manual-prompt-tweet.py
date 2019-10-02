e3b5b99ee81979eb5338df401339d48f08e71ec7
Caleb Ely <le717@users.noreply.github.com>
2019-10-02 11:14:11 -0400

    Small adjustment to manual prompt adding

--- manual-prompt-tweet.py
+++ manual-prompt-tweet.py
@@ -27,37 +27,45 @@ def extract_tweet_id(url: str) -> str:
     )[1]
 
 
-# Get the base information we need from the user to start
-tweet_date = input("Enter the tweet date (YYYY-MM-DD): ")
-tweet_url = input("Enter the tweet url: ")
-tweet_id = extract_tweet_id(tweet_url)
-user_handle = extract_handle(tweet_url)
-
-# Connect to the Twitter API and get the specified tweet
-# We're assuming/trusting the url is to the prompt tweet so we'll
-# not perform validity checks
+# Connect to the Twitter API
 api = create_twitter_connection()
-prompt_tweet = api.get_status(tweet_id, tweet_mode="extended")
 
-# Extract the tweet content
-media_url, tweet_media = get_tweet_media(prompt_tweet)
-tweet_text = get_tweet_text(prompt_tweet, media_url)
-del media_url
+# Keep prompting for tweets until told to stop
+while True:
+    # Get the base information we need from the user to start
+    tweet_date = input("Enter the tweet date (YYYY-MM-DD): ")
 
-# Construct a tweet object
-tweet = {
-    "tweet_id": tweet_id,
-    "date": create_date(tweet_date.strip()),
-    "uid": get_uid_by_handle(user_handle),
-    "handle": user_handle,
-    "content": escape(tweet_text),
-    "word": find_prompt_word(tweet_text),
-    "media": tweet_media
-}
-pprint(tweet)
+    # Stop adding prompts
+    if tweet_date.lower() == "exit":
+        raise SystemExit
 
-# Add the tweet to the database and send the emails
-print("Adding tweet to database")
-add_tweet_to_db(tweet)
-print("Sending out notification emails")
-send_emails(tweet)
+    tweet_url = input("Enter the tweet url: ")
+    tweet_id = extract_tweet_id(tweet_url)
+    user_handle = extract_handle(tweet_url)
+
+    # We're assuming/trusting the url is to the prompt tweet
+    # so we'll skip the validity checks
+    prompt_tweet = api.get_status(tweet_id, tweet_mode="extended")
+
+    # Extract the tweet content
+    media_url, tweet_media = get_tweet_media(prompt_tweet)
+    tweet_text = get_tweet_text(prompt_tweet, media_url)
+    del media_url
+
+    # Construct a tweet object
+    tweet = {
+        "tweet_id": tweet_id,
+        "date": create_date(tweet_date.strip()),
+        "uid": get_uid_by_handle(user_handle),
+        "handle": user_handle,
+        "content": escape(tweet_text),
+        "word": find_prompt_word(tweet_text),
+        "media": tweet_media
+    }
+    pprint(tweet)
+
+    # Add the tweet to the database and send the emails
+    print("Adding tweet to database")
+    add_tweet_to_db(tweet)
+    print("Sending out notification emails")
+    send_emails(tweet)
