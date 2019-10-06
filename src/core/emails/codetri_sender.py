3f83e0a5a4d836f7795e7692e45a39f011b1764f
Caleb Ely <le717@users.noreply.github.com>
2019-10-06 08:49:56 -0400

    Rewrite the emails to be instances of EmailMessage

--- src/core/emails/codetri_sender.py
+++ src/core/emails/codetri_sender.py
@@ -12,19 +12,18 @@ def rewrite_email_structure(msg: dict) -> EmailMessage:
     addr_to = msg["To"][0]["Email"].split("@")
 
     # Rebuild the email message to be an EmailMessage instance
-    msg = EmailMessage()
-    msg["subject"] = msg["Subject"]  # TODO This is not being set? Why not?
-    msg["from"] = Address("#vss365 today", "noreply", "vss365today.com")
-    msg["to"] = Address("#vss365 today Subscriber", addr_to[0], addr_to[1])
-    # msg.set_content(msg["HTMLPart"], subtype="html")  # TODO this is raising a NoneType error
-    return msg
+    em = EmailMessage()
+    em["subject"] = msg["Subject"]
+    em["from"] = Address("#vss365 today", "noreply", "vss365today.com")
+    em["to"] = Address("#vss365 today Subscriber", addr_to[0], addr_to[1])
+    em.set_content(msg["HTMLPart"], subtype="html")
+    return em
 
 
 def send_emails_codetri(msgs: list):
     # Rewrite the emails to be in the correct format
-    r = rewrite_email_structure(msgs[0])
-    # msgs = [
-    #     rewrite_email_structure(msg)
-    #     for msg in msgs
-    # ]
+    msgs = [
+        rewrite_email_structure(msg)
+        for msg in msgs
+    ]
     pass
