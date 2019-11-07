7557ceedfe34c809f202dc760ef7e1554873b77c
Caleb Ely <le717@users.noreply.github.com>
2019-11-07 13:49:53 -0500

    Alter mailjet script to work again

--- src/core/emails/mailjet_sender.py
+++ src/core/emails/mailjet_sender.py
@@ -1,10 +1,7 @@
-from random import randrange
-
 from mailjet_rest import Client
 
-from src.core.config import load_app_config, load_json_config
+from src.core.config import load_app_config
 from src.core.database import get_mailing_list
-from src.core.emails.codetri_sender import send_emails_codetri
 from src.core.emails.generator import render_email
 from src.core.filters import format_date
 
@@ -31,7 +28,6 @@ def construct_email(tweet: dict, addr: str, completed_email: str) -> dict:
 def send_emails(tweet: dict):
     # Connect to the Mailjet Send API
     CONFIG = load_app_config()
-    CONFIG_JSON = load_json_config()
     mailjet = Client(auth=(
         CONFIG["MJ_APIKEY_PUBLIC"],
         CONFIG["MJ_APIKEY_PRIVATE"]
@@ -39,7 +35,7 @@ def send_emails(tweet: dict):
 
     # Properly format the tweet date
     tweet["date"] = format_date(tweet["date"])
-    completed_email = render_email(tweet)
+    completed_email = render_email(tweet)["html"]
 
     # Get the email address list and break it into chunks of 50
     # The MailJet Send API, under a free account,
@@ -50,7 +46,7 @@ def send_emails(tweet: dict):
     # That said, if there is ever > 200 emails,
     # MailJet just won't cut it anymore. :sad_face:
     chunk_size = 50
-    email_list = get_mailing_list()
+    email_list = list(get_mailing_list())
     email_list = [
         email_list[i:i + chunk_size]
         for i in range(0, len(email_list), chunk_size)
@@ -64,13 +60,6 @@ def send_emails(tweet: dict):
             msg = construct_email(tweet, addr, completed_email)
             email_data["Messages"].append(msg)
         rendered_emails.append(email_data)
-
-    # If enabled, take out a random chunk of emails to be sent out using
-    # a new, self-hosted postfix server.
-    # These will be sent out after MailJet messages are sent
-    if CONFIG_JSON["use_new_mail_sending"]:
-        random_chunk = randrange(0, len(rendered_emails))
-        experimental_send_list = rendered_emails.pop(random_chunk)["Messages"]
 
     # Send the Mailjet emails
     for email_data in rendered_emails:
@@ -98,7 +87,3 @@ def send_emails(tweet: dict):
             # Everything wasn't successful, display raw count dict
             else:
                 print(status)
-
-    # Finally, send out the experimental emails if need be
-    if CONFIG_JSON["use_new_mail_sending"]:
-        send_emails_codetri(experimental_send_list)
