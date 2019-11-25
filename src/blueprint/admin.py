55420323f2368446f338349ab2ce383ffd3b111b
Caleb Ely <le717@users.noreply.github.com>
2019-11-25 16:53:16 -0500

    Stub in some admin routes

--- src/blueprint/admin.py
+++ src/blueprint/admin.py
@@ -2,9 +2,29 @@ from flask import Blueprint
 from flask import abort
 
 
-bp = Blueprint("admin", __name__, url_prefix="")
+bp = Blueprint("admin", __name__, url_prefix="/admin")
 
 
-@bp.route("/admin")
+@bp.route("/")
 def index():
+    abort(404)
+
+
+@bp.route("/config")
+def config():
+    abort(404)
+
+
+@bp.route("/prompts/edit/<str:prompt_date>")
+def prompt_edit(prompt_date: str):
+    abort(404)
+
+
+@bp.route("/writers")
+def writers():
+    abort(404)
+
+
+@bp.route("/writers/edit/<int:writer_id>")
+def writer_edit(writer_id: int):
     abort(404)
