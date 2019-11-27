c8b6f2cb9dbc422adfd22fce01ae8dbd35612bae
Caleb Ely <le717@users.noreply.github.com>
2019-11-27 17:12:18 -0500

    Implement direct date search

--- src/blueprint/search.py
+++ src/blueprint/search.py
@@ -1,6 +1,7 @@
-from flask import Blueprint
-from flask import abort, render_template
+from datetime import date
 
+from flask import Blueprint, request
+from flask import abort, redirect, render_template, url_for
 
 from src.core.form import PromptSearchForm
 
@@ -8,7 +9,7 @@ from src.core.form import PromptSearchForm
 bp = Blueprint("search", __name__, url_prefix="/search")
 
 
-@bp.route("/")
+@bp.route("/", methods=["GET"])
 def index():
     abort(404)
     render_opts = {
@@ -17,6 +18,20 @@ def index():
     return render_template("search/search.html", **render_opts)
 
 
-@bp.route("/<string:word>")
-def word_search(word: str):
+@bp.route("/form", methods=["POST"])
+def query_search():
     abort(404)
+
+    # We got a valid form submission
+    search_form = PromptSearchForm()
+    if search_form.validate_on_submit():
+        query = request.form.get("query").strip()
+
+        try:
+            # We recieved an exact (and valid) date, redirect to it
+            date.fromisoformat(query)
+            return redirect(url_for("root.date", date=query))
+
+        # We got a word or partial word to search
+        except ValueError:
+            pass
