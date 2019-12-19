49e8f80a75508d33edc4bc7d9c7c5b9ca2975fd7
Caleb Ely <le717@users.noreply.github.com>
2019-12-18 19:26:17 -0500

    Move log to /var/log

--- run-app.sh
+++ run-app.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-gunicorn --bind 0.0.0.0:5000 --workers 2 --log-level error --error-logfile /app/log/error.log wsgi:app
+gunicorn --bind 0.0.0.0:5000 --workers 2 --log-level error --error-logfile /var/log/error.log wsgi:app
