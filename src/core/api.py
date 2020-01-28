80d0e1a553b27ca2f239324597afebb581a82abf
Caleb Ely <le717@users.noreply.github.com>
2020-01-27 22:26:13 -0500

    Improve type hints on API methods

--- src/core/api.py
+++ src/core/api.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict
+from typing import Any, Dict, List, Union
 
 import jwt
 import requests
@@ -22,28 +22,40 @@ def create_auth_token(payload: Dict[str, Any]) -> dict:
     return {"Authorization": b"Bearer " + token}
 
 
-def get(*args: str, **kwargs: Any) -> Any:
+def get(*args: str, **kwargs: Any) -> Union[
+    List[Any],
+    Dict[str, Any]
+]:
     url = __create_api_url(*args)
     r = requests.get(url, **kwargs)
     r.raise_for_status()
     return r.json() if r.text else {}
 
 
-def post(*args: str, **kwargs: Any) -> Any:
+def post(*args: str, **kwargs: Any) -> Union[
+    List[Any],
+    Dict[str, Any]
+]:
     url = __create_api_url(*args)
     r = requests.post(url, **kwargs)
     r.raise_for_status()
     return r.json() if r.text else {}
 
 
-def put(*args: str, **kwargs: Any) -> Any:
+def put(*args: str, **kwargs: Any) -> Union[
+    List[Any],
+    Dict[str, Any]
+]:
     url = __create_api_url(*args)
     r = requests.put(url, **kwargs)
     r.raise_for_status()
     return r.json() if r.text else {}
 
 
-def delete(*args: str, **kwargs: Any) -> Any:
+def delete(*args: str, **kwargs: Any) -> Union[
+    List[Any],
+    Dict[str, Any]
+]:
     url = __create_api_url(*args)
     r = requests.delete(url, **kwargs)
     r.raise_for_status()
