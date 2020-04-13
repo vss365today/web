aafcd2f7784e8765caf953b6004590f6bf0c9cbe
Caleb Ely <le717@users.noreply.github.com>
2020-04-13 13:25:05 -0400

    Docstrings

--- src/blueprint/shortcuts.py
+++ src/blueprint/shortcuts.py
@@ -5,9 +5,11 @@ from src.blueprint import bp_shortcuts as shortcuts
 
 @shortcuts.route("today")
 def today():
+    """Shortcut link to latest prompt."""
     return redirect(url_for("root.index"))
 
 
 @shortcuts.route("privacy")
 def privacy():
+    """Shortcut link to site privacy notice."""
     return redirect(url_for("root.about", _anchor="privacy"))
