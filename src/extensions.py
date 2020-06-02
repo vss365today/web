8c3a5af89e8ba57f1b47da986bf53f1f2712248c
Caleb Ely <le717@users.noreply.github.com>
2020-06-02 10:25:04 -0400

    Use flask-google-fonts ext

--- src/extensions.py
+++ src/extensions.py
@@ -1,8 +1,11 @@
+from flask_google_fonts import GoogleFonts
 from flask_wtf.csrf import CSRFProtect
 
 csrf = CSRFProtect()
+fonts = GoogleFonts()
 
 
 def init_extensions(app):
     """Load app extensions."""
     csrf.init_app(app)
+    fonts.init_app(app)
