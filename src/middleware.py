3b05a15cf0784a50b753fb27497b5637dfca2e2f
Caleb Ely <le717@users.noreply.github.com>
2021-03-09 14:19:06 -0500

    Add get_static_url middleware method

--- src/middleware.py
+++ src/middleware.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from typing import Callable, Dict
 
-from flask import current_app, render_template
+from flask import current_app, render_template, url_for
 
 
 @current_app.context_processor
@@ -20,12 +20,29 @@ def nav_cur_page() -> Dict[str, Callable]:
 
 @current_app.context_processor
 def create_url() -> Dict[str, Callable]:
-    def _make(prompt: dict) -> str:
+    def _func(prompt: dict) -> str:
         return "https://twitter.com/{0}/status/{1}".format(
             prompt["writer_handle"], prompt["id"]
         )
 
-    return {"create_url": _make}
+    return {"create_url": _func}
+
+
+@current_app.context_processor
+def get_static_url() -> Dict[str, Callable]:
+    def _func(filename: str) -> str:
+        # TODO Docstring
+        # If this config key is present, we are running in prod,
+        # which means we should pull the files from a URL
+        if (static_url := current_app.config.get("STATIC_FILES_URL")) is not None:
+            return f"{static_url}/{filename}"
+
+        # Otherwise, we're running locally, so we pull the files
+        # from the local filesystem
+        else:
+            return url_for("static", filename=filename)
+
+    return {"get_static_url": _func}
 
 
 @current_app.errorhandler(404)
