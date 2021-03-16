3b7497619e73217deb2cba0ca511eee6a6387041
Caleb Ely <le717@users.noreply.github.com>
2021-03-16 13:25:23 -0400

    Adjust light/dark theme alert stying

--- src/static/js/alerts.js
+++ src/static/js/alerts.js
@@ -1,19 +1,15 @@
-(function () {
-  "use strict";
+const qAreaAlerts = document.querySelector(".area-alerts");
 
-  const qAreaAlerts = document.querySelector(".area-alerts");
+qAreaAlerts.addEventListener("click", function (e) {
+  // Did we click the close button on an alert?
+  if (e.target.matches(".msg-alert .btn-close")) {
+    // Delete the clicked alert
+    let alert = e.target.parentElement;
+    alert.remove();
 
-  qAreaAlerts.addEventListener("click", function (e) {
-    // Did we click the close button on an alert?
-    if (e.target.matches(".msg-alert .btn-close")) {
-      // Delete the clicked alert
-      let alert = e.target.parentElement;
-      alert.remove();
-
-      // Delete the container once all messages are dismissed
-      if (qAreaAlerts.childElementCount === 0) {
-        qAreaAlerts.remove();
-      }
+    // Delete the container once all messages are dismissed
+    if (qAreaAlerts.childElementCount === 0) {
+      qAreaAlerts.remove();
     }
-  });
-})();
+  }
+});
