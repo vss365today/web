be9018a7138c1bb8a6efeaea4ec6f1982a9eb333
Caleb Ely <le717@users.noreply.github.com>
2021-03-16 13:13:00 -0400

    Initial rework of alert display

--- src/static/js/alerts.js
+++ src/static/js/alerts.js
@@ -4,25 +4,16 @@
   const qAreaAlerts = document.querySelector(".area-alerts");
 
   qAreaAlerts.addEventListener("click", function (e) {
+    // Did we click the close button on an alert?
     if (e.target.matches(".msg-alert .btn-close")) {
-      let thisNotif = e.target.parentElement;
-      thisNotif.classList.add("hidden");
+      // Delete the clicked alert
+      let alert = e.target.parentElement;
+      alert.remove();
 
-      // Perform a fade out transition before removing the notification
-      thisNotif.addEventListener(
-        "transitionend",
-        function (e) {
-          if (e.propertyName === "opacity") {
-            e.target.remove();
-
-            // Delete the container once all messages are dismissed
-            if (qAreaAlerts.childElementCount === 0) {
-              qAreaAlerts.remove();
-            }
-          }
-        },
-        { once: true }
-      );
+      // Delete the container once all messages are dismissed
+      if (qAreaAlerts.childElementCount === 0) {
+        qAreaAlerts.remove();
+      }
     }
   });
 })();
