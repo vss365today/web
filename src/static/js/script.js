4ff72675d9b35bbc6ec7c6dddd470220a6470fe1
Caleb Ely <le717@users.noreply.github.com>
2021-03-16 23:30:45 -0400

    Update dark theme JS

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,29 +1,22 @@
-(function () {
-  "use strict";
-  // Remove the old storage key
-  const STORAGE_KEY = "is-dark-theme";
-  window.localStorage.removeItem(STORAGE_KEY);
+// Detect if the user wants a dark theme
+const themeDark = window.matchMedia("(prefers-color-scheme: dark)");
 
-  // Detect if the user wants a dark theme
-  const themeDark = window.matchMedia("(prefers-color-scheme: dark)");
+function applyLightTheme() {
+  document.body.classList.remove("dark");
+}
 
-  function applyLightTheme() {
-    document.body.classList.remove("dark");
-  }
+function applyDarkTheme() {
+  document.body.classList.add("dark");
+}
 
-  function applyDarkTheme() {
-    document.body.classList.add("dark");
-  }
+/**
+ * Used to set the dark theme on page load, if needed.
+ */
+if (themeDark.matches) {
+  applyDarkTheme();
+}
 
-  /**
-   * Used to set the dark theme on page load, if needed.
-   */
-  if (themeDark.matches) {
-    applyDarkTheme();
-  }
-
-  // Listen for theme changes
-  themeDark.addListener(function (e) {
-    (e.matches ? applyDarkTheme : applyLightTheme)();
-  });
-})();
+// Listen for theme changes
+themeDark.addEventListener("change", function (e) {
+  (e.matches ? applyDarkTheme : applyLightTheme)();
+});
