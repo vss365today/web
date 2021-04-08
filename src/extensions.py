c545ce8ef206569421d9b35acfdd7c315bf4dede
Caleb Ely <le717@users.noreply.github.com>
2021-04-07 21:35:25 -0400

    Stop loading removed extension

--- src/extensions.py
+++ src/extensions.py
@@ -1,11 +1,8 @@
-from flask_google_fonts import GoogleFonts
 from flask_wtf.csrf import CSRFProtect
 
 csrf = CSRFProtect()
-fonts = GoogleFonts()
 
 
 def init_extensions(app):
     """Load app extensions."""
     csrf.init_app(app)
-    fonts.init_app(app)
