013050c004c7ec2448a907c2444d9656635c68de
Caleb Ely <le717@users.noreply.github.com>
2021-07-15 22:01:23 -0400

    Load search.js as a module

--- src/static/js/search.js
+++ src/static/js/search.js
@@ -1,50 +1,43 @@
-(function () {
+function findParentElement(element, selector) {
   "use strict";
-
-  function findParentElement(element, selector) {
-    "use strict";
-    // The desired element was not found on the page
-    if (element === null) {
-      return null;
-    }
-
-    // We found the desired element
-    if (element.matches(selector)) {
-      return element;
-
-      // Keep searching for the element
-    } else {
-      return findParentElement(element.parentElement, selector);
-    }
+  // The desired element was not found on the page
+  if (element === null) {
+    return null;
   }
 
-  // Create the ability to toggle among search tabs
-  const qSearchTabs = document.querySelector(".form-search-tabs");
-  qSearchTabs.addEventListener("click", function (e) {
-    // Find the clicked tab
-    let ele = findParentElement(
-      e.target,
-      ".form-search-tabs .tab:not(.active)"
-    );
-    if (ele) {
-      // Switch the active tab
-      document
-        .querySelector(".form-search-tabs .tab.active")
-        .classList.remove("active");
-      ele.classList.add("active");
+  // We found the desired element
+  if (element.matches(selector)) {
+    return element;
 
-      // Show the desired search form
-      let searchType = ele.classList[1];
-      document.querySelector(".form-search.active").classList.remove("active");
-      document
-        .querySelector(".form-search." + searchType)
-        .classList.add("active");
-    }
-  });
+    // Keep searching for the element
+  } else {
+    return findParentElement(element.parentElement, selector);
+  }
+}
 
-  // Create a prettier select element
-  tail.select(document.querySelector("#input-search-host"), {
-    search: true,
-    placeholder: "ArthurUnkTweets",
-  });
-})();
+// Create the ability to toggle among search tabs
+const qSearchTabs = document.querySelector(".form-search-tabs");
+qSearchTabs.addEventListener("click", function (e) {
+  // Find the clicked tab
+  let ele = findParentElement(e.target, ".form-search-tabs .tab:not(.active)");
+  if (ele) {
+    // Switch the active tab
+    document
+      .querySelector(".form-search-tabs .tab.active")
+      .classList.remove("active");
+    ele.classList.add("active");
+
+    // Show the desired search form
+    let searchType = ele.classList[1];
+    document.querySelector(".form-search.active").classList.remove("active");
+    document
+      .querySelector(".form-search." + searchType)
+      .classList.add("active");
+  }
+});
+
+// Create a prettier select element
+tail.select(document.querySelector("#input-search-host"), {
+  search: true,
+  placeholder: "ArthurUnkTweets",
+});
