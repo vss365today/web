55b8c6ce0ece72d4ea08cf44cd080fede2a42765
Caleb Ely <le717@users.noreply.github.com>
2023-11-18 15:06:40 -0500

    Remove placeholder text in host search

--- src/static/js/search.js
+++ src/static/js/search.js
@@ -15,28 +15,30 @@ function findParentElement(element, selector) {
 }
 
 // Create the ability to toggle among search tabs
-const qSearchTabs = document.querySelector(".form-search-tabs");
-qSearchTabs.addEventListener("click", function (e) {
-  // Find the clicked tab
-  let ele = findParentElement(e.target, ".form-search-tabs .tab:not(.active)");
-  if (ele) {
-    // Switch the active tab
-    document
-      .querySelector(".form-search-tabs .tab.active")
-      .classList.remove("active");
-    ele.classList.add("active");
+document
+  .querySelector(".form-search-tabs")
+  ?.addEventListener("click", function (e) {
+    // Find the clicked tab
+    let ele = findParentElement(
+      e.target,
+      ".form-search-tabs .tab:not(.active)"
+    );
+    if (ele) {
+      // Switch the active tab
+      document
+        .querySelector(".form-search-tabs .tab.active")
+        ?.classList.remove("active");
+      ele.classList.add("active");
 
-    // Show the desired search form
-    let searchType = ele.classList[1];
-    document.querySelector(".form-search.active").classList.remove("active");
-    document
-      .querySelector(".form-search." + searchType)
-      .classList.add("active");
-  }
-});
+      // Show the desired search form
+      let searchType = ele.classList[1];
+      document.querySelector(".form-search.active")?.classList.remove("active");
+      document
+        .querySelector(".form-search." + searchType)
+        ?.classList.add("active");
+    }
+  });
 
 // Create a prettier select element
-tail.select(document.querySelector("#input-search-host"), {
-  search: true,
-  placeholder: "ArthurUnkTweets",
-});
+// @ts-ignore
+tail.select(document.querySelector("#input-search-host"), { search: true });
